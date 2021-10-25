fd7e1fd18dccb8d24c434b7883cee314d0f4a3a0
AdrijanaNj <adrijananjezic@gmail.com>
2021-10-25 22:59:11 +0200

    Added JS to search input

--- src/index.js
+++ src/index.js
@@ -42,9 +42,18 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "4b3503b2f08a729413c4d33ef1186004";
 
-let apiKey = "4b3503b2f08a729413c4d33ef1186004";
-let city = "Kristiansand";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("Kristiansand");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
