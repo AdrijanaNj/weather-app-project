5bd88393f96d32b591ad27582d7885eb44446151
AdrijanaNj <adrijananjezic@gmail.com>
2021-10-25 19:34:02 +0200

    Removed console.log

--- src/index.js
+++ src/index.js
@@ -38,5 +38,5 @@ function displayTemperature(response) {
 }
 let apiKey = "4b3503b2f08a729413c4d33ef1186004";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kristiansand&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+
 axios.get(apiUrl).then(displayTemperature);
