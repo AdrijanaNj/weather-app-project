d07cb658a8648d5e7c076961160cd592e4b6061d
AdrijanaNj <adrijananjezic@gmail.com>
2021-10-25 19:58:19 +0200

    Added relevant icon from API

--- src/index.js
+++ src/index.js
@@ -35,8 +35,16 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#day");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+
 let apiKey = "4b3503b2f08a729413c4d33ef1186004";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kristiansand&appid=${apiKey}&units=metric`;
+let city = "Kristiansand";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
